c8c9909905268ef32731d89882a12a3ce6457d7e
Wayne (Tugboat) Schroer <wayneschroer@protonmail.com>
2024-01-05 10:09:12 -0600

    Initial commit

---
+++ src/test/java/org/haib/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haib.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
